463d31dda377d8e7d296d28148b3bb74d7149e90
Yamasaki Junpei <opss0622@gmail.com>
2023-04-23 00:50:11 +0000

    feat: コンテンツを拡大する機能を追加

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,37 +1,44 @@
-import React, { useState } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-	faUpDownLeftRight,
-	faDownLeftAndUpRightToCenter
-} from '@fortawesome/free-solid-svg-icons';
+  faUpDownLeftRight,
+  faDownLeftAndUpRightToCenter,
+} from "@fortawesome/free-solid-svg-icons";
 
 export function Button(): JSX.Element {
-	const [isExpanded, setIsExpanded] = useState<Boolean>(false);
+  const [isExpanded, setIsExpanded] = useState<Boolean>(false);
 
-	function toggleExpanded(e: React.MouseEvent<HTMLButtonElement>) {
-		const parentNode = e.currentTarget.parentNode;
-		const grandParentNode = parentNode?.parentNode;
-		console.log(grandParentNode);
-		setIsExpanded(!isExpanded);
-	}
+  function toggleExpanded(e: React.MouseEvent<HTMLButtonElement>) {
+    setIsExpanded(!isExpanded);
 
-	// function checkParentNodeId() {}
+    const parent = e.currentTarget.parentNode;
+    const header = parent?.parentNode;
+    const contents = header?.parentElement;
+    const brotherElement =
+      contents?.id === "editor"
+        ? contents?.nextElementSibling
+        : contents?.previousElementSibling;
 
-	return (
-		<button
-			type="button"
-			onClick={(e) => toggleExpanded(e)}
-			style={{
-				padding: 0,
-				backgroundColor: 'transparent',
-				border: 'none',
-				cursor: 'pointer'
-			}}>
-			<FontAwesomeIcon
-				icon={!isExpanded ? faUpDownLeftRight : faDownLeftAndUpRightToCenter}
-				size="2xl"
-				transform={!isExpanded ? { rotate: 45 } : { rotate: 0 }}
-			/>
-		</button>
-	);
+    contents?.classList.toggle("is-active");
+    brotherElement?.classList.toggle("is-hidden");
+  }
+
+  return (
+    <button
+      type="button"
+      onClick={(e) => toggleExpanded(e)}
+      style={{
+        padding: 0,
+        backgroundColor: "transparent",
+        border: "none",
+        cursor: "pointer",
+      }}
+    >
+      <FontAwesomeIcon
+        icon={!isExpanded ? faUpDownLeftRight : faDownLeftAndUpRightToCenter}
+        size="2xl"
+        transform={!isExpanded ? { rotate: 45 } : { rotate: 0 }}
+      />
+    </button>
+  );
 }
